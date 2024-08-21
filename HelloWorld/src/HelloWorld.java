1d39dd1002c8d9e262240a27f5845e3d3fa5a43e
Schwartz <S03100100@ccc.ccofc.edu>
2024-08-21 14:42:41 -0600

    Import Scanner class and test scanner ability

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,8 +1,23 @@
+// Import Scanner class
+import java.util.Scanner;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World.");
-		System.out.println("My name is Zach.");
-	}
+		// Create new Scanner object from Scanner class
+		Scanner scanObj = new Scanner(System.in);
 
+		System.out.println("Hello World.");
+		System.out.println("My name is Zach.\n");
+		System.out.println("What is your name?");
+
+		String newName = scanObj.nextLine();
+		sayName(newName);
+
+		scanObj.close();
+	}
+	
+	public static void sayName(String name) {
+		System.out.println("Hello, " + name + ".");
+	}
 }
