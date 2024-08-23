22b0acfe782343a20bd2c61183f0cbcd6746f50f
ZachatorCodes <zachatorcodes@gmail.com>
2024-08-23 13:55:12 -0600

    Add info about comments

---
+++ CSC1060 Notes/src/Comments.java
@@ -0,0 +1,24 @@
+
+public class Comments {
+	
+	public static void main(String[] args) {
+		
+		// This is a single line comment
+		
+		/*
+		 * This is a multi-line comment
+		 */
+		
+		System.out.print("This is just print\n"); // The \n is an escape character indicating a new line
+		System.out.println("This is print line");
+		
+		/*
+		 * ESCAPE SEQUENCES:
+		 * \n - New Line
+		 * \t - Tab
+		 * \" Double Quote
+		 * \\ - Backslash
+		 */
+	}
+
+}
