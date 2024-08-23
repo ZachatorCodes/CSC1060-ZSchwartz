3d39f78ce56abca1898d158506ebf9501fd6c81f
ZachatorCodes <zachatorcodes@gmail.com>
2024-08-23 14:44:29 -0600

    Finish class notes/work

--- Math/src/Variables.java
+++ Math/src/Variables.java
@@ -3,7 +3,21 @@ public class Variables {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		// Declaration
+		int num1;
+		// Assignment
+		num1 = 5;
+		num1 = 6;
+		// NOTE: The final keyword makes it so that variables cannot be assigned.
+		final int finalNum1 = 5;
+		
+		// Initialization is declaration + assignment;
+		
+		// String Concatenation (Using the + operator to combine different variables with a String.
+		System.out.println("The number is \"" + num1 + "\"");
+		
+		System.out.println(String.format("The number is %s", num1));
 	}
 
 }
