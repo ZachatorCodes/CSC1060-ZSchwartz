46634e199f0809bce1b659d80de861274cfd229f
ZachatorCodes <zachatorcodes@gmail.com>
2024-08-23 14:24:25 -0600

    Class notes

---
+++ CSC1060 Notes/src/VariablesNotes.java
@@ -0,0 +1,15 @@
+
+public class VariablesNotes {
+	
+	// A variable is a named location in memory that stores a value.
+	// You must declare a variable before you use it. Variables can be final (JS const).
+	// In Java, variables start with a data type.
+	
+	// whenNamingAVariableUseCamelCase
+	// Example: numStudents, dogNames, etc.
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
