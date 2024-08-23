1020d12f7b585828f4251ef9741ecfdf3cb88b71
ZachatorCodes <zachatorcodes@gmail.com>
2024-08-23 14:48:52 -0600

    Add notes

--- Math/src/Variables.java
+++ Math/src/Variables.java
@@ -9,6 +9,9 @@ public class Variables {
 		// Assignment
 		num1 = 5;
 		num1 = 6;
+		
+		int num2 = 4;
+		int num3 = num1 + num2;
 		// NOTE: The final keyword makes it so that variables cannot be assigned.
 		final int finalNum1 = 5;
 		
@@ -17,7 +20,7 @@ public class Variables {
 		// String Concatenation (Using the + operator to combine different variables with a String.
 		System.out.println("The number is \"" + num1 + "\"");
 		
-		System.out.println(String.format("The number is %s", num1));
+		System.out.println(String.format("The number is %s", num3));
 	}
 
 }
