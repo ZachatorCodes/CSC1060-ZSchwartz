33dacc5ef9b86fc116ce7d54239da1bf4fa507b9
ZachatorCodes <zachatorcodes@gmail.com>
2024-08-23 14:13:19 -0600

    Finish basic game

--- RandomNumberGuesser/src/GuesserGame.java
+++ RandomNumberGuesser/src/GuesserGame.java
@@ -4,20 +4,29 @@ import java.util.Scanner;
 public class GuesserGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Set up required objects
 		Random randomObj = new Random();
-		int randomNum = randomObj.nextInt(0, 10);
+		int randomNum = randomObj.nextInt(0, 100);
 		Scanner scannerObj = new Scanner(System.in);
 		
-		System.out.println("Welcome to the guessing game! What is your guess?");
+		// Run game
 		game(randomNum, scannerObj);
 	}
 	
 	public static void game(int randomNum, Scanner scannerObj) {
+		System.out.println("Welcome to the guessing game! What is your guess?");
 		int guess = scannerObj.nextInt();
 		while (guess != randomNum) {
-			System.out.println("Ooh good try, but that's not it! Try again");
+			if (guess > randomNum) {
+				System.out.println("Your number is a little too high! Try again!");
+				guess = scannerObj.nextInt();
+			}
+			else {
+				System.out.println("Your number is a little too low! Try again!");
+				guess = scannerObj.nextInt();
+			}
 		}
+		System.out.println("Congrats, you got it! The number was " + randomNum + ".");
 	}
 
 }
