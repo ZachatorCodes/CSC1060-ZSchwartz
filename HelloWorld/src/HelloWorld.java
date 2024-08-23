ec51344d95537bcfa17cd6c60ac1ce3c04762df9
ZachatorCodes <zachatorcodes@gmail.com>
2024-08-23 13:34:57 -0600

    Add logic handling for no input

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -6,18 +6,26 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// Create new Scanner object from Scanner class
 		Scanner scanObj = new Scanner(System.in);
-
+		
 		System.out.println("Hello World.");
 		System.out.println("My name is Zach.\n");
 		System.out.println("What is your name?");
 
 		String newName = scanObj.nextLine();
+		
 		sayName(newName);
 
 		scanObj.close();
 	}
 	
 	public static void sayName(String name) {
-		System.out.println("Hello, " + name + ".");
+		if (name.length() == 0) {
+			System.out.println("You never gave me your name!");
+		}
+	
+		else {
+			System.out.println("Hello, " + name + ".");			
+		}
 	}
+	
 }
