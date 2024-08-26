feb3a229091b02be57f6fe55499b4100ad423fac
ZachatorCodes <zachatorcodes@gmail.com>
2024-08-26 13:29:49 -0600

    Learn about Java mathmatical operators

--- Math/src/Variables.java
+++ Math/src/Variables.java
@@ -1,4 +1,3 @@
-
 public class Variables {
 
 	public static void main(String[] args) {
@@ -7,20 +6,27 @@ public class Variables {
 		// Declaration
 		int num1;
 		// Assignment
-		num1 = 5;
 		num1 = 6;
-		
+		// Initialization
 		int num2 = 4;
-		int num3 = num1 + num2;
+		int numAdd = num1 + num2;
+		int numSub = num1 - num2;
+		double numDiv = num1 / num2;
+		int numMultiply = num1 * num2;
+		
+		System.out.println("The addition result is: " + numAdd);
+		System.out.println("The subtraction result is: " + numSub);
+		System.out.println("The division result is: " + numDiv);
+		System.out.println("The multiplication result is: " + numMultiply);
+		
 		// NOTE: The final keyword makes it so that variables cannot be assigned.
 		final int finalNum1 = 5;
-		
-		// Initialization is declaration + assignment;
+		System.out.println(finalNum1);
 		
 		// String Concatenation (Using the + operator to combine different variables with a String.
 		System.out.println("The number is \"" + num1 + "\"");
 		
-		System.out.println(String.format("The number is %s", num3));
+		System.out.println(String.format("The number is %s", numAdd));
 	}
 
 }
