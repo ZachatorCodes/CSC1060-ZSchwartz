915465b198dccc52205f2cf4bf0ae160f73d0dfc
ZachatorCodes <zachatorcodes@gmail.com>
2024-08-28 13:35:59 -0600

    Finish program to calculate roots of quadratic program

--- Math/src/QuadraticRoots.java
+++ Math/src/QuadraticRoots.java
@@ -2,7 +2,32 @@
 public class QuadraticRoots {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Initialize variables
+		int valueA;
+		int valueB;
+		int valueC;
+		
+		// Assign values to variables
+		valueA = 3;
+		valueB = 2;
+		valueC = -1;
+		
+		// Calculate the value within the square root
+		double rootValue = Math.sqrt((valueB * valueB) - (4.0 * valueA * valueC));
+		
+		// Add root value to B
+		double positiveRoot = (valueB * -1.0) + rootValue;
+		
+		// Subtract root value from B
+		double negativeRoot = (valueB * -1.0) - rootValue;
+		
+		// Calculate Answer
+		double positiveAnswer = positiveRoot / (2 * valueA);
+		double negativeAnswer = negativeRoot / (2 * valueA);
+		
+		System.out.println(positiveAnswer);
+		System.out.println(negativeAnswer);
 		
 	}
 
