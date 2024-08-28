4470ff51bb5c807924f0962d65a774c6f9fc7f8b
ZachatorCodes <zachatorcodes@gmail.com>
2024-08-28 13:45:24 -0600

    Finish TemperatureConversion assignment

---
+++ TemperatureConversion/src/TempCalculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TempCalculator {
+
+	public static void main(String[] args) {
+		Scanner scannerObj = new Scanner(System.in);
+		
+		System.out.println("Please enter a temperature in Farenheit:");
+		
+		double farenheitTemp = scannerObj.nextDouble();
+		
+		double celsiusTemp = (farenheitTemp - 32) * (5.0 / 9.0);
+		
+		double kelvinTemp = celsiusTemp + 273.15;
+		
+		System.out.println(String.format("%s degrees Farenheit is %s degrees Celsius and %s degrees Kelvin.", farenheitTemp, celsiusTemp, kelvinTemp));
+		
+		scannerObj.close();
+	}
+
+}
