20e93cd69e67fe08aeaf777bc0becba53974c00b
ZachatorCodes <zachatorcodes@gmail.com>
2024-08-28 14:22:54 -0600

    Capture temperature as double

--- Input/src/Echo.java
+++ Input/src/Echo.java
@@ -12,6 +12,10 @@ public class Echo {
 		System.out.println("How old are you?");
 		int userAge = scannerObj.nextInt();
 		System.out.printf("You are %s years old.\n", userAge);
+		
+		System.out.println("What is the temperature today?");
+		double dayTemp = scannerObj.nextDouble();
+		System.out.printf("The temperature today is %s degrees.\n", dayTemp);
 	}
 
 }
