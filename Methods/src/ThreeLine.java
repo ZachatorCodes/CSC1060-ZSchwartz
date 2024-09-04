4e6e26bde810579c2a3c086f553f96a1f0e05c70
ZachatorCodes <zachatorcodes@gmail.com>
2024-09-04 13:19:28 -0600

    Create new method for sysout lines

--- Methods/src/ThreeLine.java
+++ Methods/src/ThreeLine.java
@@ -1,9 +1,18 @@
 
 public class ThreeLine {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("First");
+		printLines();
+		System.out.println("Second");
+		printLines();
+		System.out.println("Third");
+		printLines();
+		System.out.println("Fourth");
 	}
 
+	public static void printLines() {
+		System.out.println();
+		System.out.println();
+		System.out.println();
+	}
 }
