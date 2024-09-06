e319d54f444476b0e6c1f6b4de44fb791c66f7fc
ZachatorCodes <zachatorcodes@gmail.com>
2024-09-06 14:07:15 -0600

    Add method to handle printing greeting

--- Conditionals/src/Greeting.java
+++ Conditionals/src/Greeting.java
@@ -12,9 +12,13 @@ public class Greeting {
 		int userAge = scannerObj.nextInt();
 		
 		if (userAge > 18) {
-			System.out.printf("Hello %s. You are %d years old.", userName, userAge);
+			printGreeting(userName, userAge);
 		}
 
+	}
+	
+	public static void printGreeting(String name, int age) {
+		System.out.printf("Hello %s. You are %d years old.", name, age);
 	}
 
 }
