67ea2d20df0e6300687ca949a8d97055564805ce
ZachatorCodes <zachatorcodes@gmail.com>
2024-09-06 14:08:58 -0600

    Adjust method

--- Conditionals/src/Greeting.java
+++ Conditionals/src/Greeting.java
@@ -11,14 +11,12 @@ public class Greeting {
 		System.out.println("How old are you?");
 		int userAge = scannerObj.nextInt();
 		
-		if (userAge > 18) {
-			printGreeting(userName, userAge);
-		}
-
+		printGreeting(userName, userAge);
 	}
 	
 	public static void printGreeting(String name, int age) {
-		System.out.printf("Hello %s. You are %d years old.", name, age);
+		if (age > 18) {
+			System.out.printf("Hello %s. You are %d years old.", name, age);
+		}
 	}
-
 }
