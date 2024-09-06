3f92337255feb1f4d5c95efb8e2d129b98737af7
ZachatorCodes <zachatorcodes@gmail.com>
2024-09-06 14:00:30 -0600

    Add try-catch block to even number checker

--- Conditionals/src/Positive.java
+++ Conditionals/src/Positive.java
@@ -4,17 +4,27 @@ public class Positive {
 
 	public static void main(String[] args) {
 		Scanner scannerObj = new Scanner(System.in);
-		
-		System.out.println("Please enter a number and I'll tell you if it's even, odd, or zero!");
-		int num = scannerObj.nextInt();
-		
-		if (num == 0) {
-			System.out.println("Your number is 0 exactly.");
-		} else if (num % 2 == 0) {
-			System.out.println("Your number is even.");
-		} else {
-			System.out.println("Your number is odd.");
-		}
-	}
+		boolean errorBool = true;
+		int num;
 
+		System.out.println("Please enter a number and I'll tell you if it's even, odd, or zero!");
+		
+		while (errorBool) {
+			try {
+				num = scannerObj.nextInt();	
+				if (num == 0) {
+					System.out.println("Your number is 0 exactly.");
+				} else if (num % 2 == 0) {
+					System.out.println("Your number is even.");
+				} else {
+					System.out.println("Your number is odd.");
+				}
+				errorBool = false;
+			} catch (Exception e) {
+				System.out.println("I want an integer number! Please try again.");
+				scannerObj.next();
+			}
+		}
+		
+	}
 }
