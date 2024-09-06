75168c1d27cceddd32e4963f343e614e2ba8ce0d
ZachatorCodes <zachatorcodes@gmail.com>
2024-09-06 13:33:29 -0600

    Finish in class even or odd assignment

---
+++ Conditionals/src/Module.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Module {
+
+	public static void main(String[] args) {
+		Scanner scannerObj = new Scanner(System.in);
+		
+		System.out.println("Please enter a number:");
+		int num = scannerObj.nextInt();
+		
+		if (num % 2 == 0) {
+			System.out.println("Your number is even");
+		}
+		else if (num % 2 == 1) {
+			System.out.println("Your number is odd");
+		}
+	}
+}
