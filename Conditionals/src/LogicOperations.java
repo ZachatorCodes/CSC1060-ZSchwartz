0e6cec244225e03cf3bea6536f8f9a274d8b0845
ZachatorCodes <zachatorcodes@gmail.com>
2024-09-09 13:21:34 -0600

    Format class

--- Conditionals/src/LogicOperations.java
+++ Conditionals/src/LogicOperations.java
@@ -3,27 +3,30 @@ import java.util.Scanner;
 public class LogicOperations {
 
 	public static void main(String[] args) {
-		
+
 		// Create Scanner Object
 		Scanner scnObj = new Scanner(System.in);
-		
+
 		// Prompt user for input and store as an int
 		System.out.println("Please enter an integer value into the computer:");
 		int inputValue = scnObj.nextInt();
-		
+
 		// Run only if user input is greater than 10
 		if (inputValue > 10) {
 			System.out.println("Greater Than 10");
 		}
-		
+
 		// Run only if user input is greater than 10 AND less than or equal to 20
 		if (inputValue > 10 && inputValue <= 20) {
 			System.out.println("Greater Than 10 AND Less Than Or Equal To 20");
 		}
-		
-		// If input is greater than 30 AND less than 50 OR less than -1 AND greater than -6
+
+		// If input is greater than 30 AND less than 50 OR less than -1 AND greater than
+		// -6
 		if ((inputValue > 30 && inputValue < 50) || (inputValue < -1 && inputValue > -6)) {
 			System.out.println("In Range");
+		} else {
+			System.out.println("Out Of Range");
 		}
 	}
 
