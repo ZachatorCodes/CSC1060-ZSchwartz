721f8c9f76cc21c754db7cb0f73055ad84181550
ZachatorCodes <zachatorcodes@gmail.com>
2024-09-09 14:11:54 -0600

    Create second solution using switch cases

--- Methods/src/Exercise4.java
+++ Methods/src/Exercise4.java
@@ -4,15 +4,35 @@ public class Exercise4 {
 
 	public static void main(String[] args) {
 		Scanner scnObj = new Scanner(System.in);
-		
+
 		System.out.println("Please pick an integer value to represent a day of the week (1 - 7)");
-		
+
 		System.out.println(getWeekDay(scnObj.nextInt()));
 	}
-	
+
 	public static String getWeekDay(int day) {
-		String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+		String[] daysOfWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 		return daysOfWeek[day - 1];
 	}
 
+	public static String getWeekDaySwitch(int day) {
+		switch (day) {
+		case 1:
+			return "Sunday";
+		case 2:
+			return "Monday";
+		case 3:
+			return "Tuesday";
+		case 4:
+			return "Wednesday";
+		case 5:
+			return "Thursday";
+		case 6:
+			return "Friday";
+		case 7:
+			return "Saturday";
+		default:
+			return "Error";
+		}
+	}
 }
