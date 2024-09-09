71a82ddd1d9d5c04f27f1c433ad3723805347ac2
ZachatorCodes <zachatorcodes@gmail.com>
2024-09-09 14:51:33 -0600

    Created a CodingBat repo

---
+++ CodingBat/src/Map2.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+
+public class Map2 {
+
+	public static void main(String[] args) {
+
+	}
+	
+	public Map<String, Integer> word0(String[] strings) {
+		Map<String, String> map = new HashMap<String, String>();
+	}
+
+}
