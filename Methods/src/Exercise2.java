5f0c2ff5b86adab390b1f77a204d4d8f83a3fdd6
ZachatorCodes <zachatorcodes@gmail.com>
2024-09-09 13:44:24 -0600

    Finish in class exercise 2

---
+++ Methods/src/Exercise2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		Scanner scnObj = new Scanner(System.in);
+		
+		System.out.println("Please input three integer values to determine which one is the greatest:");
+		
+		// Print the returned value of the greatestNumber method
+		System.out.println(greatestNumber(scnObj.nextInt(), scnObj.nextInt(), scnObj.nextInt()));
+	}
+	
+	public static int greatestNumber(int num1, int num2, int num3) {
+		int maxNum = num1;
+		if (num2 > maxNum) {
+			maxNum = num2;
+		}
+		if (num3 > maxNum) {
+			maxNum = num3;
+		}
+		return maxNum;
+	}
+}
