29e3b062e5c0020ef2d5f3b4ee3135f741cae97d
ZachatorCodes <zachatorcodes@gmail.com>
2024-09-09 13:56:23 -0600

    Finish in class exercise 4

---
+++ Methods/src/Exercise4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercise4 {
+
+	public static void main(String[] args) {
+		Scanner scnObj = new Scanner(System.in);
+		
+		System.out.println("Please pick an integer value to represent a day of the week (1 - 7)");
+		
+		System.out.println(getWeekDay(scnObj.nextInt()));
+	}
+	
+	public static String getWeekDay(int day) {
+		String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+		
+		return daysOfWeek[day - 1];
+	}
+
+}
