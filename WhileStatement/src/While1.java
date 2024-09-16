ac1883a35acf5074b5b5ffe81a5943212c19abce
ZachatorCodes <zachatorcodes@gmail.com>
2024-09-16 13:28:58 -0600

    Finish while assignment 1

--- WhileStatement/src/While1.java
+++ WhileStatement/src/While1.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
 
 public class While1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scnObj = new Scanner(System.in);
 
+		System.out.println("What is your name?");
+		String name = scnObj.nextLine();
+
+		System.out.println("What is your age?");
+		int age = scnObj.nextInt();
+
+		while (age < 0) {
+			System.out.println("Sorry! Your age cannot be negative.");
+			System.out.println("What is your age?");
+			age = scnObj.nextInt();
+		}
+		
 	}
-
+	
 }
