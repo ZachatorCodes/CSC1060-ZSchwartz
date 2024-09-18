c615ef8ebe60156f4413560016a56271bbfb1cfb
ZachatorCodes <zachatorcodes@gmail.com>
2024-09-18 14:52:31 -0600

    Finish HashBox assignment with while loop

--- LoopPractice/src/HashBoxWhile.java
+++ LoopPractice/src/HashBoxWhile.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
 
 public class HashBoxWhile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scnObj = new Scanner(System.in);
 
+		System.out.println("Enter height:");
+		int height = scnObj.nextInt();
+
+		System.out.println("Enter width:");
+		int width = scnObj.nextInt();
+
+		int i = height;
+		int j = width;
+
+		while (i > 0) {
+			while (j > 0) {
+				System.out.print('#');
+				j--;
+			}
+			i--;
+			j = width;
+			System.out.println();
+		}
 	}
-
 }
