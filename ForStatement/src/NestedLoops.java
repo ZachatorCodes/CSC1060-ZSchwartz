f2ca5842b246ea817d685fa0398c9cd1d9edb83e
ZachatorCodes <zachatorcodes@gmail.com>
2024-09-18 14:11:16 -0600

    Finish Nested Loop in class assignment

---
+++ ForStatement/src/NestedLoops.java
@@ -0,0 +1,12 @@
+
+public class NestedLoops {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Value of i = " + i);
+			for (int j = 0; j < 5; j++) {
+				System.out.println("Value of j = " + j);
+			}
+		}
+	}
+}
