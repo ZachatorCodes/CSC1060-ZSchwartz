39a9e29d9db745292ad1093639b5bfdb4c4fe61b
ZachatorCodes <zachatorcodes@gmail.com>
2024-09-18 13:20:30 -0600

    Finish in class assignment

---
+++ ForStatement/src/Factorial.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// Create Scanner object
+		Scanner scnObj = new Scanner(System.in);
+
+		// Prompt user for starting number and store variable
+		System.out.println("What number would you like to find the factorial of?");
+		int num = scnObj.nextInt();
+
+		// Set total number to one (not 0 because of multiplication)
+		int totalNum = 1;
+
+		// Return 1 if starting number is 0.
+		if (num == 0) {
+			System.out.println("The factorial of 0 is 1.");
+		}
+		// If number is not zero, calculate the factorial and then print the result
+		else {
+			for (int i = num; i >= 1; i--) {
+				totalNum *= i;
+			}
+			System.out.printf("The factorial of %d is %d.", num, totalNum);
+		}
+	}
+}
---
+++ ForStatement/src/SimpleCounter.java
@@ -0,0 +1,10 @@
+
+public class SimpleCounter {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			System.err.println(i);
+		}
+	}
+
+}
