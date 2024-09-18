e9e78f1646399727b2bf1031e982bc263abe6906
ZachatorCodes <zachatorcodes@gmail.com>
2024-09-18 14:36:39 -0600

    Solve with while loop

--- LoopPractice/src/FactorialWhile.java
+++ LoopPractice/src/FactorialWhile.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
 
 public class FactorialWhile {
 	
 	public static void main(String[] args) {
-		System.out.println("Test");
+		// Create Scanner object
+		Scanner scnObj = new Scanner(System.in);
+
+		// Prompt user for starting number and store variable
+		System.out.println("What number would you like to find the factorial of?");
+		int num = scnObj.nextInt();
+
+		// Set total number to one (not 0 because of multiplication)
+		int productNum = 1;
+		
+		// Create incrementor
+		int incrementor = num;
+		
+		// Return 1 if starting number is 0.
+		if (num == 0) {
+			System.out.println("The factorial of 0 is 1.");
+		}
+		// If number is not zero, calculate the factorial and then print the result
+		else {
+			while (incrementor >= 1) {
+				productNum *= incrementor;
+				incrementor--;
+			}
+			System.out.printf("The factorial of %d is %d.", num, productNum);
+		}
 	}
 }
