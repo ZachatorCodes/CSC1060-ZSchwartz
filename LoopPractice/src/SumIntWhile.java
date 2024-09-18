fa7fff36aefc258b55787fd37bbb3c43884307d6
ZachatorCodes <zachatorcodes@gmail.com>
2024-09-18 14:19:51 -0600

    Solve with while loop

---
+++ LoopPractice/src/SumIntWhile.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class SumIntWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scnObj = new Scanner(System.in);
+
+		System.out.println("Please enter a number:");
+		int userNum = scnObj.nextInt();
+
+		int totalNum = 0;
+		int incrementor = 1;
+		StringBuilder strBuilder = new StringBuilder();
+		
+		while (incrementor <= userNum) {
+			if (incrementor == 1) {
+				strBuilder.append(incrementor);
+			} else {
+				strBuilder.append(" + ").append(incrementor);
+			}
+			totalNum += incrementor;
+			incrementor++;
+		}
+
+		strBuilder.append(" = ").append(totalNum);
+
+		String finalString = strBuilder.toString();
+		System.out.println(finalString);
+	}
+
+}
