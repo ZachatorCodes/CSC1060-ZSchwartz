e93e38435dc46346f7d8867a4264490a5096f9bb
ZachatorCodes <zachatorcodes@gmail.com>
2024-09-19 15:48:17 -0600

    Format code a little better

--- Triangles/src/Triangles.java
+++ Triangles/src/Triangles.java
@@ -5,40 +5,41 @@ public class Triangles {
 	public static void main(String[] args) {
 		// Create new Scanner object
 		Scanner scnObj = new Scanner(System.in);
-		
+
 		// Create a boolean to hold loop value;
 		boolean loop = true;
-		
+
 		// Loop the entire program while loop is true
 		while (loop) {
 			// Prompt user to enter triangle size and store number
 			System.out.println("Enter triangle size:");
 			int userNumber = scnObj.nextInt();
-			
+
 			// Loop if user number is out of range to select another number
 			while (userNumber < 1 || userNumber > 50) {
 				System.out.println("Error: number must be in range [1, 50]. Please try again:");
 				userNumber = scnObj.nextInt();
 			}
-			
+
 			// Prompt user to enter a character and store as char variable
 			System.out.println("Enter triangle character:");
 			char userCharacter = scnObj.next().charAt(0);
-			
+
 			// Call print triangle method to print the triangle
 			printTriangles(userNumber, userCharacter);
-			
+
 			// Prompt user if they want to print another triangle
 			System.out.println("Do you want to print another triangle? (Yes / No)");
 			char loopResponse = scnObj.next().toLowerCase().charAt(0);
-			
+
 			// Update loop boolean value based on user response
 			if (loopResponse == 'n') {
 				loop = false;
 			}
 		}
 	}
-	
+
+	// Method to actually print the triangles
 	public static void printTriangles(int userNum, char userChar) {
 		// Create a for loop to create a new row up to the number the user provided
 		for (int rows = 1; rows <= userNum; rows++) {
