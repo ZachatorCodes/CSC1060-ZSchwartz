059e5d010e875bd8527c8d02564a3d9ddfc3cd4e
ZachatorCodes <zachatorcodes@gmail.com>
2024-10-02 13:18:47 -0600

    Added solution for array reversal without new array

--- ArrayExercises/src/ArrayReverse.java
+++ ArrayExercises/src/ArrayReverse.java
@@ -5,7 +5,8 @@ public class ArrayReverse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-		System.out.println(Arrays.toString(reverseArray(nums)));
+		System.out.println("New Array Reversal: " + (Arrays.toString(reverseArray(nums))));
+		System.out.println("Original Array Reversal: " + (Arrays.toString(reverseArrayInPlace(nums))));
 		
 	}
 	
@@ -15,6 +16,15 @@ public class ArrayReverse {
 			newArray[arr.length - (i + 1)] = arr[i];
 		}
 		return newArray;
+	}
+	
+	public static int[] reverseArrayInPlace(int[] arr) {
+		for (int i = 0; i < arr.length / 2; i++) {
+			int placeholder = arr[arr.length - (1 + i)];
+			arr[arr.length - (i + 1)] = arr[i];
+			arr[i] = placeholder;
+		}
+		return arr;
 	}
 
 }
