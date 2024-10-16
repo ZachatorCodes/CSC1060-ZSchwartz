c4a0ba79bb8f6477f0cd4c26f807a5934f128d11
ZachatorCodes <zachatorcodes@gmail.com>
2024-10-16 16:02:53 -0600

    Finish Date OOP project

---
+++ DateOOP/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Date date1 = new Date();
+		Date date2 = new Date(2002, 8, 15);
+
+		System.out.println(date1);
+		System.out.println(date2);
+	}
+
+}
