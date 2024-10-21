64081628d706a032117c4139d18c17b2c83f9d79
ZachatorCodes <zachatorcodes@gmail.com>
2024-10-21 14:41:59 -0600

    Update driver class to test shuffle function

--- CardGame/src/War.java
+++ CardGame/src/War.java
@@ -2,10 +2,13 @@
 public class War {
 
 	public static void main(String[] args) {
-		Card card = new Card(5, 2);
-		Card card2 = new Card(8, 1);
 		Deck deck = new Deck();
+		System.out.println(deck);
 		
+		deck.shuffle();
+		System.out.println(deck);
+		
+		deck.shuffle();
 		System.out.println(deck);
 	}
 
