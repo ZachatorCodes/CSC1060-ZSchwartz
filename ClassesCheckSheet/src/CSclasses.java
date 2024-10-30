ccd207cb070cf2c463f08ce7b90d87ba3d7c4e33
ZachatorCodes <zachatorcodes@gmail.com>
2024-10-30 14:57:01 -0600

    Create new Student object two different ways

--- ClassesCheckSheet/src/CSclasses.java
+++ ClassesCheckSheet/src/CSclasses.java
@@ -1,10 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CSclasses {
 
 	public static void main(String[] args) {
 		// Create new array to store 6 courses
 		Course[] courseArr = new Course[6];
-		
+
 		// Create new course with default constructor
 		// Use setter methods to change data
 		courseArr[0] = new Course();
@@ -12,7 +14,7 @@ public class CSclasses {
 		courseArr[0].setNumStudents(24);
 		courseArr[0].setMaxStudents(32);
 		courseArr[0].setCredits(4);
-		
+
 		// Create new course with default constructor
 		// Use setter methods to change data
 		courseArr[1] = new Course();
@@ -20,7 +22,7 @@ public class CSclasses {
 		courseArr[1].setNumStudents(19);
 		courseArr[1].setMaxStudents(24);
 		courseArr[1].setCredits(5);
-		
+
 		// Create new course with default constructor
 		// Use setter methods to change data
 		courseArr[2] = new Course();
@@ -28,18 +30,29 @@ public class CSclasses {
 		courseArr[2].setNumStudents(22);
 		courseArr[2].setMaxStudents(26);
 		courseArr[2].setCredits(3);
-		
+
 		// Create new course using constructor
 		// Pass all info as parameter
 		courseArr[3] = new Course("CSC1061", 23, 32, 4);
 		courseArr[4] = new Course("MATH 2420", 17, 24, 5);
 		courseArr[5] = new Course("PSY1002", 30, 30, 3);
-		
+
 		// Print all courses
-		for (Course course: courseArr) {
+		for (Course course : courseArr) {
 			System.out.println(course);
+
 		}
-		
+
+		// Create new Student with default constructor
+		Student student1 = new Student();
+		student1.setFirstName("Franklin");
+		student1.setLastName("Smith");
+		student1.setStudentNumber("S10202303");
+		student1.setListOfCourses(new ArrayList<>(Arrays.asList(courseArr[0], courseArr[1])));
+
+		// Create new Student with other constructor
+		Student student2 = new Student("Frank", "Bill", "S23578167",
+				new ArrayList<>(Arrays.asList(courseArr[0], courseArr[1])));
 	}
 
 }
