bd2011ea4fb4b7bceff3c3269c180e074d4fef8c
ZachatorCodes <zachatorcodes@gmail.com>
2024-11-21 16:29:07 -0700

    Finish assignment

--- ClassesCheckSheetAssignment/src/CSclasses.java
+++ ClassesCheckSheetAssignment/src/CSclasses.java
@@ -4,15 +4,70 @@ import java.util.List;
 public class CSclasses {
 
 	public static void main(String[] args) {
+		// Create ArrayList to hold courses
 		List<Course> courses = new ArrayList<Course>();
 		
+		// Create one of each course and add the course to the ArrayList
 		InPersonCourse inPersonCourse = new InPersonCourse("MATH2410", 24, 32, 5, 223);
-		FullRemoteCourse fullRemoteCourse = new FullRemoteCourse("CSC1060", 20, 24, 4, "supercoolteacher@frcc.com");
-		RealTimeRemoteCourse realTimeRemoteCourse = new RealTimeRemoteCourse("PHI1013", 28, 32, 3, 226536);
+		courses.add(inPersonCourse);
 		
-		System.out.println(inPersonCourse);
-		System.out.println(fullRemoteCourse);
-		System.out.println(realTimeRemoteCourse);
+		FullRemoteCourse fullRemoteCourse = new FullRemoteCourse("CSC1060", 20, 24, 4, "supercoolteacher@frcc.com");
+		courses.add(fullRemoteCourse);
+		
+		RealTimeRemoteCourse realTimeRemoteCourse = new RealTimeRemoteCourse("PHI1013", 28, 32, 3, 226536);
+		courses.add(realTimeRemoteCourse);
+		
+		// Print courses using for of loop
+		for (Course course: courses) {
+			printCourseDetailsToString(course);
+		}
+		
+		// Print courses using for loop
+		for (int i = 0; i < courses.size(); i++) {
+			printCourseDetailsToString(courses.get(i));
+		}
 	}
-
+	
+	public static void printCourseDetailsToString(Course course) {
+		System.out.println(course.toString());
+	}
+	
+	public static void printCourseDetailsManually(Course tempCourse) {
+		if (tempCourse instanceof InPersonCourse) {
+			InPersonCourse course = (InPersonCourse) tempCourse;
+			System.out.println(
+				"InPersonCourse [\n"
+				+ "  roomNumber = " + course.getRoomNumber()
+				+ "\n  courseNumber = " + course.getCourseNumber()
+				+ "\n  numStudents = " + course.getNumStudents()
+				+ "\n  maxStudents = " + course.getMaxStudents()
+				+ "\n  credits = " + course.getCredits()
+				+ "\n]"
+			);
+		}
+		if (tempCourse instanceof FullRemoteCourse) {
+			FullRemoteCourse course = (FullRemoteCourse) tempCourse;
+			System.out.println(
+				"FullRemoteCourse [\n"
+				+ "  email = " + course.getEmail() 
+				+ "\n  courseNumber = " + course.getCourseNumber()
+				+ "\n  numStudents = " + course.getNumStudents()
+				+ "\n  maxStudents = " + course.getMaxStudents()
+				+ "\n  credits = " + course.getCredits()
+				+ "\n]"
+			);
+		}
+		if (tempCourse instanceof RealTimeRemoteCourse) {
+			RealTimeRemoteCourse course = (RealTimeRemoteCourse) tempCourse;
+			System.out.println(
+				"RealTimeRemoteCourse [\n"
+				+ "  zoomRoomCode = " + course.getZoomRoomCode()
+				+ "\n  courseNumber = " + course.getCourseNumber()
+				+ "\n  numStudents = " + course.getNumStudents()
+				+ "\n  maxStudents = " + course.getMaxStudents()
+				+ "\n  credits = " + course.getCredits()
+				+ "\n]"
+			);
+		}
+	}
 }
