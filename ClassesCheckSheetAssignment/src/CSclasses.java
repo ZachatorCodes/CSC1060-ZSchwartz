d991bcc7a512f9c9fc51de609067ec60d23c15cf
ZachatorCodes <zachatorcodes@gmail.com>
2024-11-21 16:07:29 -0700

    Create one of each class

--- ClassesCheckSheetAssignment/src/CSclasses.java
+++ ClassesCheckSheetAssignment/src/CSclasses.java
@@ -6,11 +6,13 @@ public class CSclasses {
 	public static void main(String[] args) {
 		List<Course> courses = new ArrayList<Course>();
 		
-		Course course = new Course();
-		System.out.println(course);
+		InPersonCourse inPersonCourse = new InPersonCourse("MATH2410", 24, 32, 5, 223);
+		FullRemoteCourse fullRemoteCourse = new FullRemoteCourse("CSC1060", 20, 24, 4, "supercoolteacher@frcc.com");
+		RealTimeRemoteCourse realTimeRemoteCourse = new RealTimeRemoteCourse("PHI1013", 28, 32, 3, 226536);
 		
-		InPersonCourse inPersonCourse = new InPersonCourse("Frcc101", 5, 20, 3, 10);
 		System.out.println(inPersonCourse);
+		System.out.println(fullRemoteCourse);
+		System.out.println(realTimeRemoteCourse);
 	}
 
 }
