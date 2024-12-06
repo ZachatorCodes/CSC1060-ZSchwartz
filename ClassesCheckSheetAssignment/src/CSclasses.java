e613266f14ec43c9aaa1e09173f1e62781a0833e
ZachatorCodes <zachatorcodes@gmail.com>
2024-12-05 22:32:30 -0700

    Create method to read courses from file

--- ClassesCheckSheetAssignment/src/CSclasses.java
+++ ClassesCheckSheetAssignment/src/CSclasses.java
@@ -1,9 +1,12 @@
+import java.awt.dnd.DropTargetContext;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class CSclasses {
 
@@ -22,16 +25,18 @@ public class CSclasses {
 		courses.add(realTimeRemoteCourse);
 		
 		// Print courses using for of loop
-		for (Course course: courses) {
-			printCourseDetailsToString(course);
-		}
+//		for (Course course: courses) {
+//			printCourseDetailsToString(course);
+//		}
 		
 		// Print courses using for loop
-		for (int i = 0; i < courses.size(); i++) {
-			printCourseDetailsToString(courses.get(i));
-		}
+//		for (int i = 0; i < courses.size(); i++) {
+//			printCourseDetailsToString(courses.get(i));
+//		}
 		
 		writeFile(courses);
+		
+		readFile("courses.csv");
 	}
 	
 	public static void printCourseDetailsToString(Course course) {
@@ -94,20 +99,22 @@ public class CSclasses {
 			fileWriter = new FileWriter(courseFile.getName(), false); // true / false value determines whether to add or reset file
 			bufferedWriter = new BufferedWriter(fileWriter);
 			
+			bufferedWriter.write("CourseType,CourseSpecificInfo,CourseNumber,NumStudents,MaxStudents,Credits\n");
+			
 			for (Course course: courseList) {
 				if (course instanceof InPersonCourse) {
 					InPersonCourse temp = (InPersonCourse) course;
-					bufferedWriter.write(String.format("%d,%s,%d,%d,%d\n",
+					bufferedWriter.write(String.format("InPerson,%d,%s,%d,%d,%d\n",
 							temp.getRoomNumber(), temp.getCourseNumber(), temp.getNumStudents(), temp.getMaxStudents(), temp.getCredits()));
 				}
 				if (course instanceof FullRemoteCourse) {
 					FullRemoteCourse temp = (FullRemoteCourse) course;
-					bufferedWriter.write(String.format("%s,%s,%d,%d,%d\n",
+					bufferedWriter.write(String.format("FullRemote,%s,%s,%d,%d,%d\n",
 							temp.getEmail(), temp.getCourseNumber(), temp.getNumStudents(), temp.getMaxStudents(), temp.getCredits()));
 				}
 				if (course instanceof RealTimeRemoteCourse) {
 					RealTimeRemoteCourse temp = (RealTimeRemoteCourse) course;
-					bufferedWriter.write(String.format("%d,%s,%d,%d,%d\n",
+					bufferedWriter.write(String.format("RealTimeRemote,%d,%s,%d,%d,%d\n",
 							temp.getZoomRoomCode(), temp.getCourseNumber(), temp.getNumStudents(), temp.getMaxStudents(), temp.getCredits()));
 				}
 			}
@@ -115,13 +122,77 @@ public class CSclasses {
 			fileWriter.close();
 		}
 		catch (IOException e) {
-			System.out.println("Error: IO Exception Caught");
+			System.out.println("Error: IO Exception Caught | Line 125");
 			System.out.println("Bye Bye :)");
 			System.exit(-1);
 		}
 	}
 	
-	public static void readFile() {
+	public static void readFile(String fileName) {
+		File courseFile = new File(fileName);
+		Scanner fileReader = null;
 		
+		try {
+			fileReader = new Scanner(courseFile);
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("Error: File Not Found | Line 139");
+			System.out.println("Bye Bye :)");
+			System.exit(-1);
+		}
+		
+		boolean header = true;
+		while (fileReader.hasNextLine()) {
+			if (header) {
+				fileReader.nextLine();
+				header = false;
+			}
+			String line = fileReader.nextLine();
+			String[] tokens = line.split(",");
+			
+			String courseType = tokens[0];
+			String courseNumber = tokens[2];
+			int numStudents = Integer.parseInt(tokens[3]);
+			int maxStudents = Integer.parseInt(tokens[4]);
+			int credits = Integer.parseInt(tokens[5]);
+			
+			if (courseType.equals("InPerson")) {
+				int roomNumber = Integer.parseInt(tokens[1]);
+				System.out.println(
+					"InPersonCourse [\n"
+					+ "  roomNumber = " + roomNumber
+					+ "\n  courseNumber = " + courseNumber
+					+ "\n  numStudents = " + numStudents
+					+ "\n  maxStudents = " + maxStudents
+					+ "\n  credits = " + credits
+					+ "\n]"
+				);
+			}
+			if (courseType.equals("FullRemote")) {
+				String email = tokens[1];
+				System.out.println(
+					"FullRemoteCourse [\n"
+					+ "  email = " + email 
+					+ "\n  courseNumber = " + courseNumber
+					+ "\n  numStudents = " + numStudents
+					+ "\n  maxStudents = " + maxStudents
+					+ "\n  credits = " + credits
+					+ "\n]"
+				);
+			}
+			if (courseType.equals("RealTimeRemote")) {
+				int zoomCode = Integer.parseInt(tokens[1]);
+				System.out.println(
+					"RealTimeRemoteCourse [\n"
+					+ "  zoomRoomCode = " + zoomCode
+					+ "\n  courseNumber = " + courseNumber
+					+ "\n  numStudents = " + numStudents
+					+ "\n  maxStudents = " + maxStudents
+					+ "\n  credits = " + credits
+					+ "\n]"
+				);
+			}
+			
+		}
 	}
 }
